29037e06ebb9d6ade028ffaf64bb50df093d59fe
xunana <3520653564@qq.com>
2024-11-06 13:24:14 +0800

    feat:更换深色

--- src/components/GitHubProductiveTime/GitHubProductiveTime.tsx
+++ src/components/GitHubProductiveTime/GitHubProductiveTime.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 interface GitHubProductiveTimeProps {
   username: string;
+  theme?: string; 
 }
 
-const GitHubProductiveTime: React.FC<GitHubProductiveTimeProps> = ({ username }) => {
-  const imageUrl = `http://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=${username}&theme=default&utcOffset=8`;
+const GitHubProductiveTime: React.FC<GitHubProductiveTimeProps> = ({ username ,theme ='default' }) => {
+  const imageUrl = `http://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=${username}&theme=${theme}&utcOffset=8`;
 
   return (
     <div style={{ textAlign: 'center', margin: '20px 0' }}>
